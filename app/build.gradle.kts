bab25f264fe582ff5132055c017f16a159e5ee4a
Mathias Kasper <mail@mathiaskasper.de>
2026-08-21 06:53:35 +0200

    Version auf 1.4 (Code 4) setzen - luecke zu 1.3 schliessen (enthaelt Klassen-, Wildcard- und Favoriten-Fixes)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "de.hamlookup.rufzeichen"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.5"
+        versionCode = 4
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
